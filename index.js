2e8086e9ba14394aca138981dcf26fecc557acd9
dishasachdeva <127543878+DishaSachdeva-02@users.noreply.github.com>
2024-04-11 11:46:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes/myroutes')
+const routes2=require('./routes/myroutes2');
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 app.use("/angular",routes)
+app.use("/member",routes2)
 app.listen(PORT , ()=>{
     console.log("Server is running at port "+PORT)
 })
